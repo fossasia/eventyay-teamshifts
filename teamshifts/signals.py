b0070f0d8032ff6c83798f4061f9049ac73b0e52
MukundC25 <officialmukundchavan@gmail.com>
2026-06-05 19:18:14 +0530

    feat: add TeamShifts component registration with top-level URL and dashboard widget signal

---
+++ teamshifts/signals.py
@@ -0,0 +1,28 @@
+from django.dispatch import receiver
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+
+from eventyay.control.signals import event_dashboard_widgets
+
+
+@receiver(event_dashboard_widgets, dispatch_uid="teamshifts_dashboard_widget")
+def teamshifts_dashboard_widget(sender, subevent=None, lazy=False, **kwargs):
+    return [
+        {
+            "content": (
+                '<div class="numwidget">'
+                '<span class="num">-</span>'
+                '<span class="text">{}</span>'
+                "</div>"
+            ).format(str(_("TeamShifts"))),
+            "display_size": "small",
+            "priority": 80,
+            "url": reverse(
+                "plugins:teamshifts:dashboard",
+                kwargs={
+                    "organizer": sender.organizer.slug,
+                    "event": sender.slug,
+                },
+            ),
+        }
+    ]
---
+++ teamshifts/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import TemplateView
+
+from eventyay.control.permissions import EventPermissionRequiredMixin
+
+
+class TeamShiftsDashboard(EventPermissionRequiredMixin, TemplateView):
+    permission = "can_change_event_settings"
+    template_name = "teamshifts/dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["event"] = self.request.event
+        return ctx

0bc0801379448ae01668bfe280570d5be136dea2
MukundC25 <officialmukundchavan@gmail.com>
2026-06-09 00:51:09 +0530

    fix ruff ci

--- teamshifts/signals.py
+++ teamshifts/signals.py
@@ -8,9 +8,7 @@ from eventyay.control.signals import event_dashboard_components, event_dashboard
 def teamshifts_dashboard_widget(sender, subevent=None, lazy=False, **kwargs):
     return [
         {
-            "content": ('<div class="numwidget"><span class="num">-</span><span class="text">{}</span></div>').format(
-                str(_("TeamShifts"))
-            ),
+            "content": ('<div class="numwidget"><span class="num">-</span><span class="text">{}</span></div>').format(str(_("TeamShifts"))),
             "display_size": "small",
             "priority": 80,
             "url": reverse(
@@ -40,17 +38,12 @@ def teamshifts_dashboard_component(sender, request=None, **kwargs):
         "</div>"
         '<div class="panel-body">'
         "<p>{description}</p>"
-        "<p>{go_to} <a href=\"{url}\">{link_text}</a></p>"
+        '<p>{go_to} <a href="{url}">{link_text}</a></p>'
         "</div>"
         "</div>"
     ).format(
         title=str(_("TeamShifts")),
-        description=str(
-            _(
-                "Manage event teams, define team roles, review team member"
-                " applications, and build a shift schedule for your event staff."
-            )
-        ),
+        description=str(_("Manage event teams, define team roles, review team member applications, and build a shift schedule for your event staff.")),
         go_to=str(_("Go to")),
         url=url,
         link_text=str(_("TeamShifts Dashboard")),

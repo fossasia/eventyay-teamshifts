6ef8af8ccc2b20c8398668238c8542b6d2384de4
MukundC25 <officialmukundchavan@gmail.com>
2026-06-08 23:22:07 +0530

    feat: add event_dashboard_components receiver for dashboard panel

--- teamshifts/signals.py
+++ teamshifts/signals.py
@@ -1,14 +1,17 @@
 from django.dispatch import receiver
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-from eventyay.control.signals import event_dashboard_widgets
+
+from eventyay.control.signals import event_dashboard_components, event_dashboard_widgets
 
 
 @receiver(event_dashboard_widgets, dispatch_uid="teamshifts_dashboard_widget")
 def teamshifts_dashboard_widget(sender, subevent=None, lazy=False, **kwargs):
     return [
         {
-            "content": ('<div class="numwidget"><span class="num">-</span><span class="text">{}</span></div>').format(str(_("TeamShifts"))),
+            "content": ('<div class="numwidget"><span class="num">-</span><span class="text">{}</span></div>').format(
+                str(_("TeamShifts"))
+            ),
             "display_size": "small",
             "priority": 80,
             "url": reverse(
@@ -20,3 +23,35 @@ def teamshifts_dashboard_widget(sender, subevent=None, lazy=False, **kwargs):
             ),
         }
     ]
+
+
+@receiver(event_dashboard_components, dispatch_uid="teamshifts_dashboard_component")
+def teamshifts_dashboard_component(sender, request=None, **kwargs):
+    url = reverse(
+        "plugins:teamshifts:dashboard",
+        kwargs={
+            "organizer": sender.organizer.slug,
+            "event": sender.slug,
+        },
+    )
+    return (
+        '<div class="panel panel-default widget-container widget-small no-padding last-column">'
+        '<div class="panel-heading">'
+        '<h3 class="panel-title">{title}</h3>'
+        "</div>"
+        '<div class="panel-body">'
+        "<p>{description}</p>"
+        '<p><a href="{url}">{link_text}</a></p>'
+        "</div>"
+        "</div>"
+    ).format(
+        title=str(_("TeamShifts")),
+        description=str(
+            _(
+                "Manage event teams, define team roles, review team member"
+                " applications, and build a shift schedule for your event staff."
+            )
+        ),
+        url=url,
+        link_text=str(_("Go to TeamShifts Dashboard")),
+    )

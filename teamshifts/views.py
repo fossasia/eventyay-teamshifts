d0dc2a5f767a92f71054386e568830c9c3b11e78
MukundC25 <officialmukundchavan@gmail.com>
2026-07-06 18:11:53 +0530

    fix: enforce plugin-active check on public apply views (CORE_MODULES bypass)

--- teamshifts/views.py
+++ teamshifts/views.py
@@ -375,6 +375,8 @@ class PublicApplyView(FormView):
     template_name = "teamshifts/apply.html"
 
     def dispatch(self, request, *args, **kwargs):
+        if "teamshifts" not in request.event.get_plugins():
+            raise Http404
         if not request.event.live:
             raise Http404
         if not request.user.is_authenticated:
@@ -450,6 +452,8 @@ class PublicApplyThanksView(TemplateView):
     template_name = "teamshifts/apply_thanks.html"
 
     def dispatch(self, request, *args, **kwargs):
+        if "teamshifts" not in request.event.get_plugins():
+            raise Http404
         if not request.event.live:
             raise Http404
         if not request.user.is_authenticated:
